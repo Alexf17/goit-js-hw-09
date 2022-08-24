afec274d6109436ca37b24e3f0a5766780ddf2a5
Oleksii <achili3f17@gmail.com>
2022-08-24 23:50:26 +0300

    1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,33 @@
+import '../css/common.css';
+
+
+const TIMEOUT_CHANGE_COLOR = 1000
+
+const startRandom = document.querySelector('button[data-start]')
+const stopRandom = document.querySelector('button[data-stop]')
+const body = document.querySelector('body')
+let timerId = null
+startRandom.disabled = false
+
+
+startRandom.addEventListener("click", onStartClick)
+stopRandom.addEventListener("click", onStopClick)
+  
+function onStartClick() {
+  startRandom.setAttribute("disabled", "disabled");
+  timerId = setInterval(() => {
+    let randomColor = getRandomHexColor()
+    body.style.backgroundColor = randomColor
+  }, TIMEOUT_CHANGE_COLOR);
+}
+
+
+function onStopClick() {
+  clearInterval(timerId);
+  startRandom.removeAttribute("disabled")
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
