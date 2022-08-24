151be463db9f7b2173a8c2c8a4d7ef473c270042
Alexf17 <98961066+Alexf17@users.noreply.github.com>
2022-08-24 22:00:08 +0000

    Deploying to gh-pages from @ Alexf17/goit-js-hw-09@04566a89149b74acee07c14adf0b2528893d8494 🚀

---
+++ 01-color-switcher.b2469af4.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body"),n=null;t.disabled=!1,e.disabled=!0,t.addEventListener("click",(function(){t.setAttribute("disabled","disabled"),e.disabled=!1,n=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));d.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.b2469af4.js.map
